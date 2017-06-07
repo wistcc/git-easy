390d067aa643a882e5807447c2dbb35734c5ba6d
wistcc <wistcc@hotmail.com>
2017-06-07 00:17:40 -0400

    Fixing error when using other dirve on Windows

--- src/core/command.js
+++ src/core/command.js
@@ -3,5 +3,12 @@ const child_process = require('child_process');
 
 exports.exec = (path, selectedConsole) => {
     const command = consoles.get()[selectedConsole].command;
-    child_process.exec(`cd / && cd ${path} && ${command}`);
+    let fullCommand = `cd / && cd ${path} && ${command}`;
+
+    if(/^win/.test(process.platform)) {
+        const drive = path.substr(0,2);
+        fullCommand = `${drive} && cd ${path} && ${command}`;
+    }
+
+    child_process.exec(fullCommand);
 }
