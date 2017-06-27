ff9d514580ba0e424dfb2e6c1260e8a00c4a720c
Winner Crespo <wistcc@hotmail.com>
2017-06-27 06:07:50 -0800

    Fixing Mac command (#26)
    * Fixing Mac command
    
    * Fix

--- src/core/command.js
+++ src/core/command.js
@@ -2,12 +2,19 @@ const consoles = require('./consoles');
 const child_process = require('child_process');
 
 exports.exec = (path, selectedConsole) => {
+    const platform = process.platform;
     const command = consoles.get()[selectedConsole].command;
-    let fullCommand = `cd / && cd ${path} && ${command}`;
+    let fullCommand = '';
 
-    if (/^win/.test(process.platform)) {
+    if (/^win/.test(platform)) {
         const drive = path.substr(0,2);
         fullCommand = `${drive} && cd ${path} && ${command}`;
+    }
+    if (/^linux/.test(platform)) {
+        fullCommand = `cd / && cd ${path} && ${command}`;
+    }
+    if (/^darwin/.test(platform)) {
+        fullCommand = `${command} ${path}`;
     }
 
     child_process.exec(fullCommand);
