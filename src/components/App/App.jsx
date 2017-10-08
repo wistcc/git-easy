e52ca2e1018d37c32d97029109bd49960d83b1d5
Willy Ovalle <willyovalle16@gmail.com>
2017-10-08 17:46:47 -0400

    Feature: adding webpack and friends

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, Electron!</h1>
+        <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
